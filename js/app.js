25a379f14e31785b939b577af2ba3b80d2e09f06
Alexander Barouch <ajbarouch@gmail.com>
2020-07-28 17:59:27 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(document).foundation();
+
+var menu, except, w, advertButton;
+menu = document.getElementById('main-menu');
+except = document.getElementById('main-content');
+w = window.innerWidth;
+advertButton = document.getElementById('advert-button');
+
+function clickHideMenu() {
+    menu.setAttribute("style", "display: none;");    
+}
+
+function showAdvertSection(){
+    var aSection = document.getElementById('advert-section');
+    aSection.setAttribute("style", "display: block;");    
+}
+
+window.addEventListener("resize", function() {
+    var wResize = window.innerWidth;
+    if (wResize <= 1020) {
+        except.addEventListener("click", clickHideMenu);
+    } else {
+        except.removeEventListener("click", clickHideMenu);
+    }
+});
+if (w <= 1020) {
+    except.addEventListener("click", clickHideMenu);
+}
+
+advertButton.addEventListener("click", showAdvertSection);
